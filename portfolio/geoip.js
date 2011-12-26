6db706540cb2c93c263984863f61c5108885a201
Adam Obeng <github@binaryeagle.com>
2011-12-27 00:04:59 +0100

    Added whole IP range to geoip script, removed console printing

--- portfolio/geoip.js
+++ portfolio/geoip.js
@@ -1,4 +1,4 @@
-is_mit_ip = function(ip) {
+function is_mit_ip (ip) {
 	if (ip.slice(0, 2) == '18') {return true;}
 	if (ip.slice(0, 6) == '128.30') {return true;}
 	if (ip.slice(0, 6) == '128.31') {return true;}
@@ -10,14 +10,19 @@ is_mit_ip = function(ip) {
 	if (ip.slice(0, 10) == '192.52.64.') {return true;}
 	if (ip.slice(0, 10) == '192.52.65.') {return true;}
 	if (ip.slice(0, 10) == '192.52.66.') {return true;}
-	console.log(ip.slice(0, 5));
+	if (ip.slice(0, 12) == '198.125.160.') {return true;}
+	if (ip.slice(0, 12) == '198.125.161.') {return true;}
+	if (ip.slice(0, 12) == '198.125.162.') {return true;}
+	if (ip.slice(0, 12) == '198.125.163.') {return true;}
+	if ((ip.slice(0, 8) == '198.125.') & (parseInt(ip.slice(9, 11)) < 192) & (parseInt(ip.slice(9, 11)) < 175)) {return true;}
 	return false;
 }
 
-cb = function(obj) {
+function cb(obj) {
 	ip = obj.ip;
 	
 	if (is_mit_ip(ip)){
+		console.log('MIT');
 		$('#custom_greeting').hide();
 		$('#custom_greeting').text('Hello, MIT visitor!');
 		$('body').toggleClass('mit');
